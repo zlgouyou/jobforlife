1d7897428c299fb37c1e1d6c2eefeaa66e8390a5
Maxime Andre <maxime.andr@gmail.com>
2017-10-02 08:52:52 +0200

    Initialisation projet pour prez job for life

---
+++ job-for-life/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $fr.mandre.jobforlife.job_for_life;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
